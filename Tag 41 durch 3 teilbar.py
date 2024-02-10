23b149056b3a25433329bec1c85679cd7f17dd2e
reco74 <152899081+reco74@users.noreply.github.com>
2024-02-10 03:22:31 +0100

    ordnerstruktur

---
+++ Tag 41 durch 3 teilbar.py
@@ -0,0 +1,7 @@
+zahl = int(input("Gib eine Zahl ein: "))
+
+
+if zahl % 3 == 0:
+    print(f"{zahl} ist eine durch 3 teilbare Zahl.")
+else:
+    print(f"{zahl} ist eine nicht durch 3 teilbare Zahl.")
